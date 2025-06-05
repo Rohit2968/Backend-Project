4a9034633df8ed11f684b1f0f45e4b09882fda72
Rohit2968 <rohit2968raj@gmail.com>
2025-06-05 21:42:01 +0530

    update files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import express from "express";
-import dotenv from "dotenv";
+// import express from "express";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 8000;
+// const app = express();
+// const PORT = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-  res.send("Server is running...");
-});
+// app.get("/", (req, res) => {
+//   res.send("Server is running...");
+// });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+
+import mongoose from "mongoose";
+
+;( async () => {
+  try {
+    mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+  }
+})
