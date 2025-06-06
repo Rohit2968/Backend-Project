7dcc18e8559a0ad3796634e7feaed103a206e28d
Rohit2968 <rohit2968raj@gmail.com>
2025-06-06 09:33:32 +0530

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
-// import express from "express";
-// import dotenv from "dotenv";
+import express from "express";
+import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 
-// const app = express();
-// const PORT = process.env.PORT || 8000;
+const app = express();
+const PORT = process.env.PORT || 8000;
 
-// app.get("/", (req, res) => {
-//   res.send("Server is running...");
-// });
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
-
+/*
+import express from "express"
+const app = express()
 import mongoose from "mongoose";
 
 ;( async () => {
   try {
-    mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    app.on("error", (error) => {
+      console.log("Error:", error);
+      throw error
+    })
+    app.listen(process.env.PORT, () => {
+      console.log('App is listening on port ${process.env.PORT}');
+    })
+
+
+  } catch (error) {
+    console.error("Error:", error)
+    throw err
   }
 })
+*/
